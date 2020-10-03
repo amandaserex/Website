4c2fec7ab2b89575ca5e6df2603ecb0a0a9c844d
Amanda Serex <amandaserex@ucsb.edu>
2020-10-02 22:25:13 -0700

    Update Courses Font Size

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -13,7 +13,7 @@ export class Courses extends Component {
                     ContainerClassName='ScrollUpButton__Container'
                     TransitionClassName='ScrollUpButton__Toggled'
                 />
-            <h1>Overall GPA: 3.62</h1>
+            <h2>Overall GPA: 3.62</h2>
             <h2>Computer Science Courses - 3.74 GPA</h2>
             <h3>Fall 2020</h3>
             <h5>CMPSC 130B Data Structure Algorithm II</h5>
