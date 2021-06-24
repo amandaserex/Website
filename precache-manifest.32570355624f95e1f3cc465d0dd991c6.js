e5f10730ffa2810c6abc50ba03cebf2d7043b9e7
Amanda Serex <amandaserex@ucsb.edu>
2021-06-24 11:51:49 -0700

    Updates

---
+++ precache-manifest.32570355624f95e1f3cc465d0dd991c6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd53427305ee798ea892d40e5726f4b0",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "9c43d64cd0c1515870ac",
+    "url": "/Website/static/css/main.b9f17fe2.chunk.css"
+  },
+  {
+    "revision": "e8412b036dcc8d699192",
+    "url": "/Website/static/js/2.20dbbea4.chunk.js"
+  },
+  {
+    "revision": "20a154b886b82cdb1da594a2d87e5a2d",
+    "url": "/Website/static/js/2.20dbbea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c43d64cd0c1515870ac",
+    "url": "/Website/static/js/main.1e988765.chunk.js"
+  },
+  {
+    "revision": "e82c88337471e4cd87f9",
+    "url": "/Website/static/js/runtime-main.e6e171c1.js"
+  },
+  {
+    "revision": "1758958ab26ee5f6600d567021b39347",
+    "url": "/Website/static/media/GeoPics.1758958a.jpg"
+  },
+  {
+    "revision": "b155783fdba48fb4098e77b6c6060b47",
+    "url": "/Website/static/media/Graphs.b155783f.png"
+  },
+  {
+    "revision": "1feecae4015ecb4de1c451b385970a15",
+    "url": "/Website/static/media/IMG_5727.1feecae4.jpeg"
+  },
+  {
+    "revision": "bb7a864d4fa1e9ede0d43617a742247e",
+    "url": "/Website/static/media/IMG_5940.bb7a864d.jpeg"
+  },
+  {
+    "revision": "31780ae2dda29cca8d986c82552d2561",
+    "url": "/Website/static/media/IMG_5992.31780ae2.jpeg"
+  },
+  {
+    "revision": "26fc8d35f951f0a04b94f9aa41899cdb",
+    "url": "/Website/static/media/IMG_6060.26fc8d35.jpeg"
+  },
+  {
+    "revision": "b88d014bbd797f324b361825185d96e6",
+    "url": "/Website/static/media/IMG_6070.b88d014b.jpeg"
+  },
+  {
+    "revision": "a6b00928314f5e347d027e9637e7c460",
+    "url": "/Website/static/media/IMG_7356.a6b00928.jpeg"
+  },
+  {
+    "revision": "e5d0fe173e5c9e4d88c03cfefa81fc3e",
+    "url": "/Website/static/media/IMG_7764.e5d0fe17.jpeg"
+  },
+  {
+    "revision": "1599b7b996f367c2b758e8c0433f343a",
+    "url": "/Website/static/media/MealPlannerIcon.1599b7b9.png"
+  },
+  {
+    "revision": "5fd2ca91544dbe405bef4c7735a59a77",
+    "url": "/Website/static/media/air.5fd2ca91.jpeg"
+  },
+  {
+    "revision": "544c3c59b55bea42ee621621e3c21f19",
+    "url": "/Website/static/media/blue.544c3c59.jpeg"
+  },
+  {
+    "revision": "295fd82729bb29734652c2075103ed01",
+    "url": "/Website/static/media/iv.295fd827.jpeg"
+  },
+  {
+    "revision": "6a28df6c3e10b1a52d492a1caa1e2793",
+    "url": "/Website/static/media/snow.6a28df6c.jpeg"
+  },
+  {
+    "revision": "de3805d67ac9bfbd1196602d81878053",
+    "url": "/Website/static/media/sun.de3805d6.jpeg"
+  },
+  {
+    "revision": "de74b3b86547d437bf21cbad9f6f2135",
+    "url": "/Website/static/media/sunset.de74b3b8.jpeg"
+  }
+]);
--- src/config.js
+++
@@ -1,9 +0,0 @@
-//dont worry about exposing your API key, it is bad practice and generally not advised, but for now it really doesn't matter, Google ensures only the authorized users can access api keys
-import firebase from 'firebase'
-const config = {
-    apiKey: "AIzaSyBHJb094WyUWDWhxsVyHKbP4aMqZlEHJ2Y",
-    authDomain: "cs185-hw-4.firebaseapp.com",
-    databaseURL: "https://cs185-hw-4.firebaseio.com",
-  }
-firebase.initializeApp(config);
-export default firebase;
